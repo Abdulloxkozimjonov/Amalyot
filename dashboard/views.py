ae395971cb6d57898f0983fe2126be085721f17a
Abdulloxkozimjonov <kozimjonova00@gmail.com>
2024-02-19 17:19:24 +0600

    Abdullox

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from main import models
+from main import serializers
+from rest_framework.generics import ListCreateAPIView, UpdateAPIView, DestroyAPIView
 
-# Create your views here.
+""" Start CRUD Employee """
+class CreateEmployee(ListCreateAPIView):
+     queryset = models.Employee.objects.all()
+     serializer_class = serializers.EmployeeSerializer
+
+
+class UpdateEmployee(UpdateAPIView):
+    queryset = models.Employee
+    serializer_class = serializers.EmployeeSerializer
+
+
+class DeleteEmployee(DestroyAPIView):
+    queryset = models.Employee
+    serializer_class = serializers.EmployeeSerializer
+
+""" End CRUD Employee """
+
+
