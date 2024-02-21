62f1a0754ec52af56f6016805c865eb3ee73e5e8
Abdulloxkozimjonov <kozimjonova00@gmail.com>
2024-02-21 16:30:21 +0600

    abdullox

--- accaunt/urls.py
+++ accaunt/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("sing-up-account", views.singup_view),
-    path("sing-in-view", views.singin_view),
-    path("edit_user", views.UpdateUser.as_view()),
-    path("logo-out", views.logoout),
+    path("sing-up/", views.singup_view),
+    path("sing-in/", views.singin_view),
+    path("edit_user/", views.UpdateUser.as_view()),
+    path("logo-out/", views.logoout),
 ]
