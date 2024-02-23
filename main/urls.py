6121b567aac9c268ade558e2356644e5da73faf1
Abdulloxkozimjonov <kozimjonova00@gmail.com>
2024-02-23 16:06:34 +0600

    abdullox

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-
+    path('filter-by-name/', views.employee_by_name),
+    path('filter-by-age/', views.employee_by_age),
+    path('filter-by-experience/', views.employee_by_experience)
 ]
